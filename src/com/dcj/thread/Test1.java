f39f1bb3ba56832fcc3633c8ae12d46b2c37a4ff
hellodcj <hellodcj@163.com>
2014-08-26 17:39:02 +0800

    pom

--- src/com/dcj/thread/Test1.java
+++ src/com/dcj/thread/Test1.java
@@ -1,5 +1,35 @@
 package com.dcj.thread;
 
-public class Test1 {
+import org.junit.Test;
 
+public class Test1 {
+	
+	@Test
+	public void startListOff(){
+		for (int i=1;i<=5;i++){
+			ListOff lo = new ListOff();
+			Thread t = new Thread (lo);
+			t.start();
+		}
+	}
 }
+
+
+class ListOff implements Runnable{
+	protected int countDown=10;
+	private static int tastCount=0;
+	private final int id = tastCount++;
+	
+	public String status(){
+		return "#"+id+"--"+countDown;
+	}
+
+	@Override
+	public void run(){
+		while (countDown-->0){
+			System.out.println(status());
+			//Thread.sleep(1000);
+		}
+	}
+}
+	
